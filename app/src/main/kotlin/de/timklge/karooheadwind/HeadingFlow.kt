086912117696695976c6e20e62896171ff5de417
timklge <2026103+timklge@users.noreply.github.com>
2025-01-30 18:34:06 +0100

    fix #8: Use new location change event (#31)
    * fix #8: Use new location change event
    
    * Skip location values without orientation, remove averaging

--- app/src/main/kotlin/de/timklge/karooheadwind/HeadingFlow.kt
+++ app/src/main/kotlin/de/timklge/karooheadwind/HeadingFlow.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
@@ -57,26 +58,6 @@ fun KarooSystemService.getHeadingFlow(context: Context): Flow<HeadingResponse> {
             headingValue ?: HeadingResponse.NoGps
         }
         .distinctUntilChanged()
-        .scan(emptyList<HeadingResponse>()) { acc, value -> /* Average over 3 values */
-            if (value !is HeadingResponse.Value) return@scan listOf(value)
-
-            val newAcc = acc + value
-            if (newAcc.size > 3) newAcc.drop(1) else newAcc
-        }
-        .map { data ->
-            Log.i(KarooHeadwindExtension.TAG, "Heading value: $data")
-
-            if (data.isEmpty()) return@map HeadingResponse.NoGps
-            if (data.firstOrNull() !is HeadingResponse.Value) return@map data.first()
-
-            val avgValues = data.mapNotNull { (it as? HeadingResponse.Value)?.diff }
-
-            if (avgValues.isEmpty()) return@map HeadingResponse.NoGps
-
-            val avg = avgValues.average()
-
-            HeadingResponse.Value(avg)
-        }
 }
 
 fun <T> concatenate(vararg flows: Flow<T>) = flow {
@@ -115,24 +96,13 @@ fun KarooSystemService.getGpsCoordinateFlow(context: Context): Flow<GpsCoordinat
 
     val initialFlow = flow {
         val lastKnownPosition = context.getLastKnownPosition()
-        if (lastKnownPosition != null) emit(lastKnownPosition)
+
+        emit(lastKnownPosition)
     }
 
-    val gpsFlow = streamDataFlow(DataType.Type.LOCATION)
-        .map { (it as? StreamState.Streaming)?.dataPoint?.values }
-        .map { values ->
-            val lat = values?.get(DataType.Field.LOC_LATITUDE)
-            val lon = values?.get(DataType.Field.LOC_LONGITUDE)
-            val bearing = values?.get(DataType.Field.LOC_BEARING)
-
-            if (lat != null && lon != null){
-                // Log.d(KarooHeadwindExtension.TAG, "Updated gps coordinates: $lat $lon")
-                GpsCoordinates(lat, lon, bearing)
-            } else {
-                // Log.w(KarooHeadwindExtension.TAG, "Gps unavailable: $values")
-                null
-            }
-        }
+    val gpsFlow = streamLocation()
+        .filter { it.orientation != null }
+        .map { GpsCoordinates(it.lat, it.lng, it.orientation) }
 
     val concatenatedFlow = concatenate(initialFlow, gpsFlow)
 
